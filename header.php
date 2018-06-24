468d14fc09719dde328f2f629617ac15ad320e53
manno-k <art.kuromi@gmail.com>
2018-06-24 22:41:07 +0900

    array編集

--- header.php
+++ header.php
@@ -64,10 +64,7 @@
 								menu
 							</div>
 							<?php
-							wp_nav_menu( array(
-								'theme_location' => 'menu-1',
-								'menu_id'        => 'primary-menu',
-							) );
+							wp_nav_menu();
 							?>
 						</div>
 					</div><!--/.sidebar-offcanvas-->
