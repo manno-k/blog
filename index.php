16c62c6aa890bb87fffa7a63fc96d31fe5d65036
manno-k <art.kuromi@gmail.com>
2018-06-24 21:44:38 +0900

    deploy test

--- index.php
+++ index.php
@@ -25,30 +25,11 @@ get_header(); ?>
 		);
 		$the_query = new WP_Query( $args );
 		if ( have_posts() ) :
-			while ( have_posts() ) : the_post(); ?>
-				<article>
-					<a href="<?php the_permalink() ?>">
-						<dl>
-							<dd>
-								<time>
-									<?php the_time( 'Y.m.d' ) ?>
-								</time>
-								<h1>
-									<?php the_title(); ?>
-								</h1>
-							</dd>
-							<dt>
-								<?php if ( has_post_thumbnail() ): ?>
-									<?php echo get_the_post_thumbnail( $post->ID, 'thumb100' ); ?>
-								<?php else: ?>
-									<i class="fa fa-picture-o" aria-hidden="true"></i>
-								<?php endif; ?>
-							</dt>
+			while ( have_posts() ) : the_post();
 
-						</dl>
-					</a>
-				</article>
-			<?php endwhile;
+		get_template_part('template-parts/content','archive');
+
+			endwhile;
 		endif; ?>
 
 		<?php
--- template-parts/content-archive.php
+++ template-parts/content-archive.php
@@ -1,14 +1,22 @@
 <article>
 	<a href="<?php the_permalink() ?>">
 		<dl>
-			<dt><?php the_time( 'Y.m.d' ) ?>
-				<span class="category"><?php the_category( '｜' ) ?></span>
-			</dt>
 			<dd>
+				<time>
+					<?php the_time( 'Y.m.d' ) ?>
+				</time>
 				<h1>
 					<?php the_title(); ?>
 				</h1>
 			</dd>
+			<dt>
+				<?php if ( has_post_thumbnail() ): ?>
+					<?php echo get_the_post_thumbnail( $post->ID, 'thumb100' ); ?>
+				<?php else: ?>
+					<i class="fa fa-picture-o" aria-hidden="true"></i>
+				<?php endif; ?>
+			</dt>
+
 		</dl>
 	</a>
 </article>
